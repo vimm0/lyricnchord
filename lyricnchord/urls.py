cd1c06c9949405f854af4822d055127bfa136617
Sandesh Rana (vimm0) <vimmrana0@gmail.com>
2023-07-26 08:42:05 +0545

    Fix artist api

--- lyricnchord/urls.py
+++ lyricnchord/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import serializers, viewsets, routers
 
-from apps.core.models import Song
+from apps.core.models import Song, Artist
 
 
 class SongSerializer(serializers.ModelSerializer):
@@ -16,8 +16,20 @@ class SongViewSet(viewsets.ModelViewSet):
     serializer_class = SongSerializer
 
 
+class ArtistSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Artist
+        fields = '__all__'
+
+
+class ArtistViewSet(viewsets.ModelViewSet):
+    queryset = Artist.objects.all().order_by('name')
+    serializer_class = ArtistSerializer
+
+
 router = routers.DefaultRouter()
 router.register(r'songs', SongViewSet)
+router.register(r'artists', ArtistViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
